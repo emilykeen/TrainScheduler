79125e45cf4ecb760cbdbbc88f38da41e9962118
Emily Keen <emily.keen711@gmail.com>
2017-03-17 15:11:18 -0400

    fixed to military time

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+//adding api and initializing databse
   var config = {
       apiKey: "AIzaSyDwGDZm1thh5eiu1GTu9zOQ2cY6bdU4yQ8",
       authDomain: "trainscheduler-1b4ec.firebaseapp.com",
@@ -7,22 +8,35 @@
   };
   firebase.initializeApp(config);
 
+  //setting databse to a varible
+
   var database = firebase.database();
 
+//run event when button clicked
   $("#add-train").on("click", function(event) {
+    //prevent refreshing of page when button pushed
       event.preventDefault();
-
+      //grab form input values
       var trainName = $("#name-input").val().trim();
       var destination = $("#Destination-input").val().trim();
       var firstTime = $("#first-time-input").val().trim();
       var frequency = $("#frequency-input").val().trim();
-      var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
-      var currentTime = moment();
+
+      //convert first time to determine when the next time will be
+      //subtract a year to work with the times
+      var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+      //format the current time 
+      var currentTime = moment().format("HH:mm");
+      //find the differenence in now and the first train time in minutes
       var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+      // find the remainder of the difference in now and first time 
       var tRemainder = diffTime % frequency;
+      //subtract remainder from the frequency. 
       var minutesTillTrain = frequency - tRemainder;
+      //add mintuestill next train to the current time in minutes
       var nextTrain = moment().add(minutesTillTrain, "minutes");
-      var nextTrainFormatted = moment(nextTrain).format("hh:mm");
+      //format 
+      var nextTrainFormatted = moment(nextTrain).format("HH:mm");
 
       var newTrain = {
           name: trainName,
@@ -52,6 +66,8 @@
       frequency = childSnapshot.val().frequency;
       nextTrainFormatted = childSnapshot.val().nextTrainFormatted;
       minutesTillTrain = childSnapshot.val().minutesTillTrain;
+
+      //nextTrainFormatted = moment(nextTrainFormatted).format("HHmm");
 
 
       $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" +
